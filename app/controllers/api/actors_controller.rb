4952f403f6281a84dbf4b30e0ff1894f9f3e1ab3
dani <dani.zaghian@gmail.com>
2019-01-28 12:45:47 -0800

    updates movie app

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,14 +1,49 @@
 class Api::ActorsController < ApplicationController
+	def index
+		@actors = Actor.all
+		render 'index.json.jbuilder'
+	end
+
 	def show
 		@actor = Actor.find(params[:id])
 		render 'show.json.jbuilder'
 	end
 
 	def create
-		@actor = Actor.new(
-			first_name: params[:first_name],
-			last_name: params[:last_name],
-			known_for: params[:known_for]
-		)
-	end
+    @actor = Actor.new(
+     	first_name: params[:first_name],
+     	last_name: params[:last_name],
+      known_for: params[:known_for],
+      gender: params[:gender],
+     	age: params[:age]
+    )
+
+    if @actor.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @actor = Actor.find(params[:id])
+    
+    @actor.first_name = params[:first_name] || @actor.first_name
+    @actor.last_name = params[:last_name] || @actor.last_name
+    @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
+
+    if @actor.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @actor = Actor.find(params[:id])
+    @actor.destroy
+    render json: {message: "Actor successfully destroyed"}
+  end
 end
