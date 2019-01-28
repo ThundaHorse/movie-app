d1431e75d5089006e85d3bf4e8d06418eaacb32d
dani <dani.zaghian@gmail.com>
2019-01-28 12:52:34 -0800

    adds protect from forgery

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
 	validates :title, length: { maximum: 100 }
-	validates :age, numericality: { in: 1990..2019 }
+	validates :year, numericality: { in: 1990..2019 }
 	validates :english, presence: true
 end
